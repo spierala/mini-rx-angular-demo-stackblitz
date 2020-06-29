17c66c21fc8e493be04bd959c9c4e0c080e3aa09
Florian Spier <Florian.Spier@portofantwerp.com>
2020-06-29 22:31:19 +0200

    Delete

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -7,107 +7,103 @@ import { HttpClient } from '@angular/common/http';
 const apiUrl = 'api/todos/';
 
 export class Todo {
-    id: number;
-    title: string;
-    isDone: boolean;
+  id: number;
+  title: string;
+  isDone: boolean;
 }
 
 interface TodoState {
-    todos: Todo[];
-    selectedTodoId: number;
+  todos: Todo[];
+  selectedTodoId: number;
 }
 
 const initialState: TodoState = {
-    todos: [],
-    selectedTodoId: undefined,
+  todos: [],
+  selectedTodoId: undefined,
 };
 
 @Injectable({ providedIn: 'root' })
 export class TodosService extends Feature<TodoState> {
-    todosDone$: Observable<Todo[]> = this.select((state) =>
-        state.todos.filter((item) => item.isDone)
-    );
-    todosNotDone$: Observable<Todo[]> = this.select((state) =>
-        state.todos.filter((item) => !item.isDone)
-    );
-    selectedTodo$: Observable<Todo> = this.select((state) => {
-        if (state.selectedTodoId === 0) {
-            return new Todo();
-        }
-        return state.todos.find((item) => item.id === state.selectedTodoId);
-    });
-
-    constructor(private http: HttpClient) {
-        super('todos', initialState);
-
-        this.load();
+  todosDone$: Observable<Todo[]> = this.select((state) =>
+    state.todos.filter((item) => item.isDone)
+  );
+  todosNotDone$: Observable<Todo[]> = this.select((state) =>
+    state.todos.filter((item) => !item.isDone)
+  );
+  selectedTodo$: Observable<Todo> = this.select((state) => {
+    if (state.selectedTodoId === 0) {
+      return new Todo();
     }
+    return state.todos.find((item) => item.id === state.selectedTodoId);
+  });
 
-    selectTodo(todo: Todo) {
-        this.setState({ selectedTodoId: todo.id }, 'selectTodo');
-    }
+  constructor(private http: HttpClient) {
+    super('todos', initialState);
 
-    addTodo() {
-        this.setState({ selectedTodoId: 0 });
-    }
+    this.load();
+  }
 
-    // Effects
-    load = this.createEffect(
-        switchMap(() => this.http.get<Todo[]>(apiUrl).pipe(map((todos) => ({ todos })))),
-        'load'
-    );
+  selectTodo(todo: Todo) {
+    this.setState({ selectedTodoId: todo.id }, 'selectTodo');
+  }
 
-    create = this.createEffect<Todo>(
-        switchMap((todo) => {
-            return this.http
-                .post<Todo>(apiUrl, { ...todo, id: null }) // Product Id must be null for the Web API to assign an Id
-                .pipe(
-                    map((newTodo) => ({
-                        todos: [...this.state.todos, newTodo],
-                        selectedTodoId: newTodo.id,
-                    }))
-                );
-        }),
-        'create'
-    );
+  addTodo() {
+    this.setState({ selectedTodoId: 0 });
+  }
 
-    update = this.createEffect<Todo>(
-        pipe(
-            withLatestFrom(this.selectedTodo$), // Get snapshot of selectedTodo for undoing optimistic update
-            switchMap(([todo, originalTodo]) =>
-                this.http.put<Todo>(apiUrl + todo.id, todo).pipe(
-                    map((updatedTodo) => ({
-                        todos: this.state.todos.map((item) =>
-                            item.id === todo.id ? updatedTodo : item
-                        ),
-                    })),
-                    // Undo Optimistic Update
-                    catchError(() =>
-                        of({
-                            todos: this.state.todos.map((item) =>
-                                item.id === todo.id ? originalTodo : item
-                            ),
-                        })
-                    ),
-                    // Optimistic Update
-                    startWith({
-                        todos: this.state.todos.map((item) => (item.id === todo.id ? todo : item)),
-                    })
-                )
-            )
-        ),
-        'update'
-    );
+  // Effects
+  load = this.createEffect(
+    switchMap(() => this.http.get<Todo[]>(apiUrl).pipe(map((todos) => ({ todos })))),
+    'load'
+  );
 
-    delete = this.createEffect<Todo>(
-        switchMap((todo) =>
-            this.http.delete<Todo>(apiUrl + todo.id).pipe(
-                map(() => ({
-                    selectedTodoId: undefined,
-                    todos: this.state.todos.filter((item) => item.id !== todo.id),
-                }))
-            )
-        ),
-        'delete'
-    );
+  create = this.createEffect<Todo>(
+    switchMap((todo) => {
+      return this.http
+        .post<Todo>(apiUrl, { ...todo, id: null }) // Product Id must be null for the Web API to assign an Id
+        .pipe(
+          map((newTodo) => ({
+            todos: [...this.state.todos, newTodo],
+            selectedTodoId: newTodo.id,
+          }))
+        );
+    }),
+    'create'
+  );
+
+  update = this.createEffect<Todo>(
+    pipe(
+      withLatestFrom(this.selectedTodo$), // Get snapshot of selectedTodo for undoing optimistic update
+      switchMap(([todo, originalTodo]) =>
+        this.http.put<Todo>(apiUrl + todo.id, todo).pipe(
+          map((updatedTodo) => ({
+            todos: this.state.todos.map((item) => (item.id === todo.id ? updatedTodo : item)),
+          })),
+          // Undo Optimistic Update
+          catchError(() =>
+            of({
+              todos: this.state.todos.map((item) => (item.id === todo.id ? originalTodo : item)),
+            })
+          ),
+          // Optimistic Update
+          startWith({
+            todos: this.state.todos.map((item) => (item.id === todo.id ? todo : item)),
+          })
+        )
+      )
+    ),
+    'update'
+  );
+
+  delete = this.createEffect<Todo>(
+    switchMap((todo) =>
+      this.http.delete<Todo>(apiUrl + todo.id).pipe(
+        map(() => ({
+          selectedTodoId: undefined,
+          todos: this.state.todos.filter((item) => item.id !== todo.id),
+        }))
+      )
+    ),
+    'delete'
+  );
 }
